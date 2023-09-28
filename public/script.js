5ecdaa732a0f5fb735e72137c3478ef60f22fd04
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-09-28 11:23:45 +0530

    enhancements in login page

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("join-chat");
 const userNameInput = document.getElementById("username-input");
-const userForm = document.getElementById("userForm");
+const userForm = document.getElementById("userLoginForm");
 const chatRoomContainer = document.querySelector(".chatRoom-Container");
 
 
@@ -9,11 +9,11 @@ let userName;
 
 btn.addEventListener("click", (event)=>{
     event.preventDefault();
-    chatRoomContainer.style.display = "block";
 
     userName = userNameInput.value;
 
     if(userName){
         userForm.style.display = "none";
+        chatRoomContainer.style.display = "block";
     }
 })
