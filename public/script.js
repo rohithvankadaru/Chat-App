73509864df83fb91eb8d50e61038437992bdeaee
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-09-28 20:22:42 +0530

    chat room done

--- public/script.js
+++ public/script.js
@@ -1,19 +1,56 @@
-const btn = document.getElementById("join-chat");
+var socket = io();
+let userName = "";
+const joinChatBtn = document.getElementById("join-chat");
 const userNameInput = document.getElementById("username-input");
 const userForm = document.getElementById("userLoginForm");
 const chatRoomContainer = document.querySelector(".chatRoom-Container");
+const sendBtn = document.getElementById("send-btn");
+const inputMessageTag = document.querySelector(".messageInput");
+const messagesContainer = document.querySelector(".messages");
 
 
-let userName;
 
-
-btn.addEventListener("click", (event)=>{
+joinChatBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
     userName = userNameInput.value;
 
-    if(userName){
+    if (userName) {
         userForm.style.display = "none";
         chatRoomContainer.style.display = "block";
     }
+});
+
+
+sendBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (inputMessageTag.value) {
+        let data = {
+            id: socket.id,
+            userName: userName,
+            message: inputMessageTag.value
+        };
+        socket.emit("chat message", data);
+        appendMessage(data, "sent");
+        inputMessageTag.value = null;
+    }
+});
+
+socket.on('chat message', data => {
+    if (data.id !== socket.id) {
+        appendMessage(data, "received")
+    }
 })
+
+
+function appendMessage(data, type) {
+    const msgDiv = document.createElement('div');
+    msgDiv.innerText = `${data.userName} : ${data.message}`;
+    if (type === 'sent') {
+        msgDiv.setAttribute('class', 'message sent')
+    }
+    else {
+        msgDiv.setAttribute('class', 'message received')
+    }
+    messagesContainer.append(msgDiv);
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const PORT  = 8888;
+
+io.on("connection", (socket)=>{
+    socket.on("chat message", (data)=>{
+        io.emit("chat message", data);
+    })
+});
+
+app.use(express.static("public"));
+
+
+server.listen(PORT);
