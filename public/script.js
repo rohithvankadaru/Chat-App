2eaad1094a54f206a09a08b80f89de29f2b3af7c
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-10-08 00:04:56 +0530

    added feature of sending images

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 var socket = io();
 let userName = "";
 let toUserName = "";
+let file;
 const joinChatBtn = document.getElementById("join-chat");
 const userNameInput = document.getElementById("username-input");
 const toUserNameInput = document.getElementById("toUsername-input");
@@ -9,6 +10,8 @@ const chatRoomContainer = document.querySelector(".chatRoom-Container");
 const sendBtn = document.getElementById("send-btn");
 const inputMessageTag = document.querySelector(".messageInput");
 const messagesContainer = document.querySelector(".messages");
+const fileInputBtn = document.getElementById('file-input');
+const chooseFileBtn = document.getElementById('choose-file');
 let userNameDisplay = document.getElementById('user-name-display');
 
 
@@ -22,14 +25,26 @@ joinChatBtn.addEventListener("click", (event) => {
     if (userName) {
         userForm.style.display = "none";
         chatRoomContainer.style.display = "block";
-        socket.on(`chat message ${userName}`, data => {
-            if (data.id !== socket.id) {
-                appendMessage(data, "received")
-            }
-        })
-        userNameDisplay.innerText = userName;
+        socketFn();
+        userNameDisplay.innerText = userName + ' --> ' + toUserName;
     }
 });
+
+function socketFn() {
+    socket.on(`chat message ${userName}`, data => {
+        if (data.id !== socket.id) {
+            appendMessage(data, "received");
+            const imgFile = readImgFile(data.file);
+            if (imgFile) {
+                appendFile(imgFile, 'received');
+            }
+        }
+    })
+}
+
+function readImgFile(arrayBuffer) {
+    return new Blob([arrayBuffer], { type: 'image/jpeg' });
+}
 
 inputMessageTag.addEventListener("keypress", (event) => {
     if (event.keyCode == 13) {
@@ -39,15 +54,19 @@ inputMessageTag.addEventListener("keypress", (event) => {
 
 sendBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    if (inputMessageTag.value) {
+    if (inputMessageTag.value || file) {
         let data = {
             id: socket.id,
             userName: userName,
             toUserName: toUserName,
-            message: inputMessageTag.value
+            message: inputMessageTag.value,
+            file: file
         };
         socket.emit("chat message", data);
         appendMessage(data, "sent");
+        if (data.file) {
+            appendFile(data.file, 'sent');
+        }
         inputMessageTag.value = null;
     }
 });
@@ -61,8 +80,27 @@ function appendMessage(data, type) {
         msgDiv.innerText = `${data.message}`;
     }
     else {
-        msgDiv.innerText = `${data.userName} : ${data.message}`;
         msgDiv.setAttribute('class', 'message received')
+        msgDiv.innerText = `${data.userName} : ${data.message}`;
     }
     messagesContainer.append(msgDiv);
 }
+function appendFile(imgArg, type) {
+    const imgTag = document.createElement('img');
+    imgTag.style.width = '12vw';
+    imgTag.style.height = 'auto';
+    if (type === 'sent') {
+        imgTag.setAttribute('class', 'sent chatImg');
+    } else {
+        imgTag.setAttribute('class', 'received chatImg');
+    }
+    imgTag.src = URL.createObjectURL(imgArg);
+    messagesContainer.appendChild(imgTag);
+    file = '';
+}
+
+fileInputBtn.addEventListener('click', () => chooseFileBtn.click());
+chooseFileBtn.addEventListener('change', () => {
+    file = chooseFileBtn.files[0];
+    sendBtn.click();
+})
