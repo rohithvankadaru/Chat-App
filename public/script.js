9ddd026a2ae47b93f6a504d8f940642a78709fe0
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-10-09 13:39:47 +0530

    test

--- public/script.js
+++ public/script.js
@@ -54,7 +54,8 @@ inputMessageTag.addEventListener("keypress", (event) => {
 
 inputMessageTag.addEventListener('focus', ()=>{
     if(window.innerHeight <= 1048 && window.innerWidth <= 500){
-        messagesContainer.style.height = '45vh';
+        messagesContainer.style.height = '35vh';
+        document.body.style.height = '40vh';
     }
 });
 inputMessageTag.addEventListener('blur', ()=>{
